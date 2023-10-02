8a540f7826f53a8f71772041895d8d3b5395c4d2
Tristan Labelle <tristan@thebrowser.company>
2023-10-02 14:47:23 -0400

    Fixed AsyncOperationCompletedHandler iid

--- UWP/Assembly/Generics+Projection.swift
+++ UWP/Assembly/Generics+Projection.swift
@@ -8,7 +8,7 @@ extension WindowsFoundation_AsyncOperationCompletedHandlerProjection where TResu
         public typealias CStruct = CWinRT.__FIAsyncOperationCompletedHandler_1_Windows__CFoundation__CDiagnostics__CErrorDetails
         public typealias CVTableStruct = CWinRT.__FIAsyncOperationCompletedHandler_1_Windows__CFoundation__CDiagnostics__CErrorDetailsVtbl
 
-        public static let iid = IID(0x9B05106D, 0x77E0, 0x5C24, 0x82B0, 0x9B2DC8F79671)
+        public static let iid = IID(0xA6997F9D, 0x7195, 0x5972, 0x8ECD, 0x1C73AA5CB312)
         public static var runtimeClassName: String { "Windows.Foundation.AsyncOperationCompletedHandler`1<Windows.Foundation.Diagnostics.ErrorDetails>" }
     }
 }
