667ed207d0fbe763ade1ca4bec3515e07488f616
Tristan Labelle <tristan@thebrowser.company>
2023-10-03 19:34:11 -0700

    Added QueryInterfaceTests

---
+++ Tests/QueryInterfaceTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import UWP_WindowsSecurityCryptographyCore
+import UWP_WindowsStorageStreams
+import WinRT
+
+internal final class QueryInterfaceTests: WinRTTestCase {
+    func testIUnknownIdentityRule() throws {
+        let swiftBuffer = SwiftBuffer([1, 2, 3])
+        let bufferByteAccess = try swiftBuffer.queryInterface(IBufferByteAccessProjection.self)
+        let buffer = try swiftBuffer.queryInterface(IBufferProjection.self)
+        XCTAssertEqual(
+            try bufferByteAccess.queryInterface(IUnknownProjection.self)._unknown,
+            try buffer.queryInterface(IUnknownProjection.self)._unknown)
+    }
+
+    func testTransitivityRule() throws {
+        let swiftBuffer = SwiftBuffer([1, 2, 3])
+        let unknown = try swiftBuffer.queryInterface(IUnknownProjection.self)
+        let buffer = try swiftBuffer.queryInterface(IBufferProjection.self)
+        let bufferByteAccess = try swiftBuffer.queryInterface(IBufferByteAccessProjection.self)
+
+        // QueryInterface should succeed from/to any pair of implemented interfaces
+        let objects: [COMObject] = [unknown, bufferByteAccess, buffer]
+        for object in objects {
+            _ = try object.queryInterface(IUnknownProjection.self)
+            _ = try object.queryInterface(IBufferProjection.self)
+            _ = try object.queryInterface(IBufferByteAccessProjection.self)
+        }
+    }
+}
