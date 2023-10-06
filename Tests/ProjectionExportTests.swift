071e998082688c03ffe08a61e0032f5fb388c737
Tristan Labelle <tristan@thebrowser.company>
2023-10-06 06:47:54 -0700

    Added failing IInspectable tests.

--- Tests/ProjectionExportTests.swift
+++ Tests/ProjectionExportTests.swift
@@ -14,16 +14,27 @@ internal final class ProjectionExportTests: WinRTTestCase {
             try buffer.queryInterface(IUnknownProjection.self)._unknown)
     }
 
-    func testTransitivityRule() throws {
+    func testIInspectableIdentityRule() throws {
+        let swiftBuffer = SwiftBuffer([1, 2, 3])
+        let bufferByteAccess = try swiftBuffer.queryInterface(IBufferByteAccessProjection.self)
+        let buffer = try swiftBuffer.queryInterface(IBufferProjection.self)
+        XCTAssertEqual(
+            try bufferByteAccess.queryInterface(IUnknownProjection.self)._unknown,
+            try buffer.queryInterface(IInspectableProjection.self)._unknown)
+    }
+
+    func testQueryInterfaceTransitivityRule() throws {
         let swiftBuffer = SwiftBuffer([1, 2, 3])
         let unknown = try swiftBuffer.queryInterface(IUnknownProjection.self)
+        let inspectable = try swiftBuffer.queryInterface(IInspectableProjection.self)
         let buffer = try swiftBuffer.queryInterface(IBufferProjection.self)
         let bufferByteAccess = try swiftBuffer.queryInterface(IBufferByteAccessProjection.self)
 
         // QueryInterface should succeed from/to any pair of implemented interfaces
-        let objects: [COMProjectionObjectBase] = [unknown, bufferByteAccess, buffer]
+        let objects: [COMProjectionObjectBase] = [unknown, inspectable, bufferByteAccess, buffer]
         for object in objects {
             _ = try object.queryInterface(IUnknownProjection.self)
+            _ = try object.queryInterface(IInspectableProjection.self)
             _ = try object.queryInterface(IBufferProjection.self)
             _ = try object.queryInterface(IBufferByteAccessProjection.self)
         }
